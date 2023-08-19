d5aa63027082917fe0afc8832913078b1779351f
leandrodamiao <138677356+leandrodamiao@users.noreply.github.com>
2023-08-19 13:44:07 +0000

    CursoVideoPython1.1

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ print("=" * 60)
 print(f"{'Desafios Curso em Vídeo Pyhton':^60}")
 print("=" * 60)
 print('')
-opcaodesafio = input(print('QUal desafio deseja ver? '))
 
-if opcaodesafio == '1':
-  print("=" * 60)
-  print(f"{'Desafio 1 Curso em Vídeo Pyhton':^60}")
-  print("=" * 60)
-  print('')
-  print('Olá Mundo')
+while True:
+  opcaodesafio = input(print('QUal desafio deseja ver agora? \nTecle s ou sair para finalizar o programa.'))
+  if opcaodesafio in ('s', 'sair'):
+    print('Foi um prazer tê-lo aqui, até a próxima!')
+    break
 
-else:
-  print("Infálido ou ainda não existe!")
+  if opcaodesafio == '1':
+    print("=" * 60)
+    print(f"{'Desafio 1 Curso em Vídeo Pyhton':^60}")
+    print("=" * 60)
+    print('')
+    print('Olá Mundo \n')
+
+  else:
+    print("Infálido ou ainda não existe!")
