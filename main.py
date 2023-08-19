1eb09412728f52c0ab1d83aa6485211c2196f377
leandrodamiao <138677356+leandrodamiao@users.noreply.github.com>
2023-08-19 13:15:17 +0000

    CursoVideoPython1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Olá Mundo')
+print("=" * 60)
+print(f"{'Desafios Curso em Vídeo Pyhton':^60}")
+print("=" * 60)
+print('')
+opcaodesafio = input(print('QUal desafio deseja ver? '))
+
+if opcaodesafio == '1':
+  print("=" * 60)
+  print(f"{'Desafio 1 Curso em Vídeo Pyhton':^60}")
+  print("=" * 60)
+  print('')
+  print('Olá Mundo')
+
+else:
+  print("Infálido ou ainda não existe!")
