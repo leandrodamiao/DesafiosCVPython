f9d87f09c9b7711a40b87dc236b1da9f98b89412
leandrodamiao <138677356+leandrodamiao@users.noreply.github.com>
2023-08-22 11:31:57 +0000

    CursoVideoPython_V8

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("=" * 60)
-print(f"{'Desafios Curso em Vídeo Pyhton':^60}")
-print("=" * 60)
+print("=" * 25)
+print(f"{'Desafios Pyhton':^25}")
+print("=" * 25)
 print('')
 
 while True:
@@ -13,31 +13,31 @@ while True:
     break
 
   elif opcaodesafio == '1':
-    print("=" * 60)
-    print(f"{'Desafio 1 Curso em Vídeo Pyhton':^60}")
-    print("=" * 60)
+    print("=" * 25)
+    print(f"{'D1 - Primeiro Print:':^25}")
+    print("=" * 25)
     print('')
     print('Olá Mundo')
 
   elif opcaodesafio == '2':
-    print('=' * 60)
-    print(f"{'Desafio 2 Curso em Vídeo Python:':^60}")
-    print('=' * 60)
+    print('=' * 25)
+    print(f"{'D2 - Boas Vindas:':^25}")
+    print('=' * 25)
     nome = input('Qual o seu nome? ')
     print(f"É um prazer conhecer você {nome}!")
 
   elif opcaodesafio == '3':
-    print('=' * 60)
-    print(f"{'Desafio 3 Curso em Vídeo Python:':^60}")
-    print('=' * 60)
+    print('=' * 25)
+    print(f"{'D3 - Soma:':^25}")
+    print('=' * 25)
     n1 = int(input('Informe um número: '))
     n2 = int(input('Informe outro número: '))
     print(f"A soma entre {n1} e {n2} é igual a {n1+n2}.")
 
   elif opcaodesafio == '4':
-    print('=' * 60)
-    print(f"{'Desafio 4 Curso em Vídeo Python:':^60}")
-    print('=' * 60)
+    print('=' * 25)
+    print(f"{'D4 - Tipo de Dados:':^25}")
+    print('=' * 25)
     print('')
     algo = input('Digite algo: \n')
     print(f"O tipo primitivo desse valor é {type(algo)}")
@@ -50,29 +50,39 @@ while True:
     print(f"Está capitalizada? {algo.istitle()}")
 
   elif opcaodesafio == '5':
-    print('=' * 60)
-    print(f"{'Desafio 5 Curso em Vídeo Python:':^60}")
-    print('=' * 60)
+    print('=' * 25)
+    print(f"{'D5 - Antes/Depois nº:':^25}")
+    print('=' * 25)
     print('')
     n = int(input("Digite um número inteiro: "))
     print(f"Analisando o valor {n}, o antecessor é {n-1} e o sucessor é {n+1}")
 
   elif opcaodesafio == '6':
-    print('=' * 60)
-    print(f"{'Desafio 6 Curso em Vídeo Python:':^60}")
-    print('=' * 60, '\n')
+    print('=' * 25)
+    print(f"{'D6 - Dobro/Triplo/Raiz:':^25}")
+    print('=' * 25, '\n')
     n = int(input("Digite um número: "))
     print(
       f"O dobro de {n} vale {n*2}. \nO triplo de {n} vale {n*3}. \nA raiz quadrada de {n} vale {n**(1/2):.2f}."
     )
 
   elif opcaodesafio == '7':
-    print('=' * 60)
-    print(f"{'Desafio 7 Curso em Vídeo Python:':^60}")
-    print('=' * 60, '\n')
+    print('=' * 25)
+    print(f"{'D7 - Média:':^25}")
+    print('=' * 25, '\n')
     n1 = float(input("Primeira nota do aluno: "))
     n2 = float(input("Segunda nota do aluno: "))
     print(f"A média entre {n1} e {n2} é igual a {(n1+n2)/2:.1f}")
+
+  elif opcaodesafio == '8':
+    print('=' * 25)
+    print(f"{'D8 - Conversor Medidas':^25}")
+    print('=' * 25)
+    distancia = float(input('Uma distância em metros: '))
+    print(f"A medida de {distancia:.1f}m corresponde a: ")
+    print(
+      f"{distancia/1000}km \n{distancia/100}hm \n{distancia/10}dam \n{distancia*10:.0f}dm \n{distancia*100:.0f}cm \n{distancia*1000:.0f}mm"
+    )
 
   else:
     print("Infálido ou ainda não existe!")
